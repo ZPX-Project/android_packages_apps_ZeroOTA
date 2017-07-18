2cda6d3dcdddb44702e11400ba0d74a394ce9d33
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-07-18 20:34:33 -0300

    ViperOTA: Viper Color Engine

--- Android.mk
+++ Android.mk
@@ -29,6 +29,10 @@ LOCAL_RESOURCE_DIR := \
     $(TOP)/frameworks/support/v7/recyclerview/res \
     $(LOCAL_PATH)/res
 
+# Viper Color Engine
+$(warning *** Including Viper Color Engine ***)
+LOCAL_RESOURCE_DIR += vendor/viper/colorengine/$(LOCAL_PATH)/res
+
 LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages android.support.design \
